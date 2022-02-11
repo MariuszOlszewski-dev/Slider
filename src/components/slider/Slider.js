3168d26eefa2b2584409eeecf4883334739aac2c
mariusz <mariusz.olszewski@endysoft.com>
2022-02-11 18:21:33 +0100

    Adding data

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { sliderData } from './slider-data';
 
 const Slider = () => {
     return (
         <div>
-            <h1>Slider</h1>
+            <h1>{sliderData.map((slide, i) => {
+                return(
+                    <div>
+                        {/* process.env.PUBLIC_URL - should be replaced with the address leading to the photos */}
+                        <img src={process.env.PUBLIC_URL + `/img/${i+1}.jpg`} alt="" />
+                        <h1>{slide.heading}</h1>
+                        <h3>{slide.subHeading}</h3>
+                        <p>{slide.contents}</p>
+                        <button>{slide.btn1}</button>
+                        <button>{slide.btn2}</button>
+                    </div>
+                )
+            })}</h1>
         </div>
     )
 }
--- src/components/slider/slider-data.js
+++ src/components/slider/slider-data.js
@@ -1,5 +1,8 @@
+import { v4 as uuidv4 } from 'uuid';
+
 export const sliderData = [
     {
+        id: uuidv4(),
         heading: "Quisque vulputate mi consectetur",
         subHeading: "Quisque vulputate mi consectetur",
         contents: "orem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere quis mi a fermentum. Nulla vel fringilla quam. Quisque a nulla euismod, finibus est eu, molestie lectus. Nam rhoncus nec nibh sit amet rhoncus. Donec mauris dolor, suscipit a felis auctor, dignissim imperdiet mi. Nulla facilisi. Praesent lobortis in purus in bibendum. Pellentesque eleifend, dui sit amet scelerisque hendrerit, mauris diam fermentum massa, et posuere nisl orci sed mi. In leo libero, sollicitudin eu felis in, egestas ultrices dui. Integer sed libero egestas, faucibus lectus ac, consectetur enim.",
@@ -7,6 +10,7 @@ export const sliderData = [
         btn2: "DOLOR SIT AMET"
     },
     {
+        id: uuidv4(),
         heading: "Pellentesque eleifend, dui sit amet",
         subHeading: "Mauris diam fermentum massa.",
         contents: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere quis mi a fermentum. Nulla vel fringilla quam. Quisque a nulla euismod, finibus est eu, molestie lectus. Nam rhoncus nec nibh sit amet rhoncus. Donec mauris dolor, suscipit a felis auctor, dignissim imperdiet mi. Nulla facilisi. Praesent lobortis in purus in bibendum. Pellentesque eleifend, dui sit amet scelerisque hendrerit, mauris diam fermentum massa, et posuere nisl orci sed mi. In leo libero, sollicitudin eu felis in, egestas ultrices dui. Integer sed libero egestas, faucibus lectus ac, consectetur enim.",
@@ -14,6 +18,7 @@ export const sliderData = [
         btn2: "DOLOR SIT AMET"
     },
     {
+        id: uuidv4(),
         heading: "Nam rhoncus nec nibh sit amet",
         subHeading: "Donec mauris dolor, suscipit a felis auctor.",
         contents: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere quis mi a fermentum. Nulla vel fringilla quam. Quisque a nulla euismod, finibus est eu, molestie lectus. Nam rhoncus nec nibh sit amet rhoncus. Donec mauris dolor, suscipit a felis auctor, dignissim imperdiet mi. Nulla facilisi. Praesent lobortis in purus in bibendum. Pellentesque eleifend, dui sit amet scelerisque hendrerit, mauris diam fermentum massa, et posuere nisl orci sed mi. In leo libero, sollicitudin eu felis in, egestas ultrices dui. Integer sed libero egestas, faucibus lectus ac, consectetur enim.",
