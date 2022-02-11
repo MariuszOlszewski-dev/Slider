0a77020afa214a23c82c591ad0ac6929991a9980
mariusz <mariusz.olszewski@endysoft.com>
2022-02-11 17:48:40 +0100

    Initial state

---
+++ src/components/slider/slider-data.js
@@ -0,0 +1,23 @@
+export const sliderData = [
+    {
+        heading: "Quisque vulputate mi consectetur",
+        subHeading: "Quisque vulputate mi consectetur",
+        contents: "orem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere quis mi a fermentum. Nulla vel fringilla quam. Quisque a nulla euismod, finibus est eu, molestie lectus. Nam rhoncus nec nibh sit amet rhoncus. Donec mauris dolor, suscipit a felis auctor, dignissim imperdiet mi. Nulla facilisi. Praesent lobortis in purus in bibendum. Pellentesque eleifend, dui sit amet scelerisque hendrerit, mauris diam fermentum massa, et posuere nisl orci sed mi. In leo libero, sollicitudin eu felis in, egestas ultrices dui. Integer sed libero egestas, faucibus lectus ac, consectetur enim.",
+        btn1: "LOREM IPSUM",
+        btn2: "DOLOR SIT AMET"
+    },
+    {
+        heading: "Pellentesque eleifend, dui sit amet",
+        subHeading: "Mauris diam fermentum massa.",
+        contents: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere quis mi a fermentum. Nulla vel fringilla quam. Quisque a nulla euismod, finibus est eu, molestie lectus. Nam rhoncus nec nibh sit amet rhoncus. Donec mauris dolor, suscipit a felis auctor, dignissim imperdiet mi. Nulla facilisi. Praesent lobortis in purus in bibendum. Pellentesque eleifend, dui sit amet scelerisque hendrerit, mauris diam fermentum massa, et posuere nisl orci sed mi. In leo libero, sollicitudin eu felis in, egestas ultrices dui. Integer sed libero egestas, faucibus lectus ac, consectetur enim.",
+        btn1: "LOREM IPSUM",
+        btn2: "DOLOR SIT AMET"
+    },
+    {
+        heading: "Nam rhoncus nec nibh sit amet",
+        subHeading: "Donec mauris dolor, suscipit a felis auctor.",
+        contents: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere quis mi a fermentum. Nulla vel fringilla quam. Quisque a nulla euismod, finibus est eu, molestie lectus. Nam rhoncus nec nibh sit amet rhoncus. Donec mauris dolor, suscipit a felis auctor, dignissim imperdiet mi. Nulla facilisi. Praesent lobortis in purus in bibendum. Pellentesque eleifend, dui sit amet scelerisque hendrerit, mauris diam fermentum massa, et posuere nisl orci sed mi. In leo libero, sollicitudin eu felis in, egestas ultrices dui. Integer sed libero egestas, faucibus lectus ac, consectetur enim.",
+        btn1: "LOREM IPSUM",
+        btn2: "DOLOR SIT AMET"
+    }
+]
